bbaf34576d765b25ad229592676658d500374e47
mizonov <mizonov.iv@gmail.com>
2022-12-21 16:16:57 +0500

    footer update

--- evacuation/src/components/Footer.vue
+++ evacuation/src/components/Footer.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="footer mt-auto py-3 bg-light">
-    <div class="container">
-      <span class="text-muted">Place sticky footer content here.</span>
+    <div class="container d-flex flex-column align-items-center">
+      <div class="footer__logo">
+        <img class="d-block w-100" src="../assets/icons/logo.png" alt="">
+      </div>
+      <div class="h6 text-center">
+        <p>Телефон</p>
+        <a href="">+7 (343) 207-78-38</a>
+      </div>
+      <div class="h6 text-center">
+        <p>E-mail</p>
+        <p>info@avangard196.ru</p>
+      </div>
+      <div class="h6 text-center">
+        <p>Адрес</p>
+        <p>Блюхера 58, офис 401</p>
+        <p>Титова 27/3, офис 102</p>
+        <p>Екатеринбург</p>
+      </div>
+      <div class="h6 text-center">
+        <p>Режим работы</p>
+        <p>09:00 до 18:00</p>
+      </div>
+      <div>
+        <div class="footer__logo mx-auto">
+          <img class="d-block w-100" src="../assets/icons/logo.png" alt="">
+        </div>
+        <div class="h6 text-center mt-3">
+          <p>© «Авангард» - Сервис онлайн страхования на Урале, 2010-2022.</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,5 +41,8 @@ export default {
 </script>
 
 <style scoped>
-
+.footer__logo {
+  width: 200px;
+  height: 45px;
+}
 </style>
