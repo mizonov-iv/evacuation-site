6c6c741a24f82283834767dcabb47b1c6e3a1c66
mizonov <mizonov.iv@gmail.com>
2023-01-23 10:10:04 +0500

    no message

--- evacuation/src/components/About.vue
+++ evacuation/src/components/About.vue
@@ -105,58 +105,6 @@
 
   </div>
 
-<!--  <div class="container d-flex flex-column align-items-center mt-3">-->
-<!--    <h3 class="text-center">Ключевые достоинства аварийного комиссара</h3>-->
-<!--    <p class="text-center">Экономим Ваше время и берем всю головную боль на себя!</p>-->
-
-<!--    <div class="row mb-3 mx-auto">-->
-<!--      <div class="d-flex col-md-5 ">-->
-<!--        <div class="about-icon">-->
-<!--          <img src="../assets/icons/location.svg" alt="location">-->
-<!--        </div>-->
-<!--        <p>Оформим документы на месте ДТП</p>-->
-<!--      </div>-->
-
-<!--      <div class="d-flex align-items-baseline col-md-5">-->
-<!--        <div class="about-icon">-->
-<!--          <img src="../assets/icons/magnifier.svg" alt="magnifier">-->
-<!--        </div>-->
-<!--        <p>Проведем независимую экспертизу</p>-->
-<!--      </div>-->
-
-<!--      <div class="d-flex align-items-baseline col-md-5">-->
-<!--        <div class="about-icon">-->
-<!--          <img src="../assets/icons/people.svg" alt="people">-->
-<!--        </div>-->
-<!--        <p>Урегулируем споры со страховой</p>-->
-<!--      </div>-->
-
-<!--      <div class="d-flex align-items-baseline col-md-5">-->
-<!--        <div class="about-icon">-->
-<!--          <img src="../assets/icons/photography.svg" alt="photography">-->
-<!--        </div>-->
-<!--        <p>Проведем фото-фиксацию места ДТП</p>-->
-<!--      </div>-->
-
-<!--      <div class="d-flex align-items-baseline col-md-5">-->
-<!--        <div class="about-icon">-->
-<!--          <img src="../assets/icons/mail.svg" alt="mail">-->
-<!--        </div>-->
-<!--        <p>Подготовим претензию в страховую</p>-->
-<!--      </div>-->
-
-<!--      <div class="d-flex align-items-baseline col-md-5">-->
-<!--        <div class="about-icon">-->
-<!--          <img src="../assets/icons/car.svg" alt="car">-->
-<!--        </div>-->
-<!--        <p>Проведем оценку качества ремонта</p>-->
-<!--      </div>-->
-<!--    </div>-->
-
-<!--    <button class="btn btn-sm btn-outline-success col-8 col-md-2">Заказать звонок</button>-->
-
-<!--  </div>-->
-
 </template>
 
 <script>
